35b4dbce7382f784d2149b710d7f0201d2052ffc
Kleetus MacTavish <kleetus.mactavish@gmail.com>
2026-05-03 22:26:10 -0400

    #57: Reworked version.h to avoid the version codes and version string possibly getting out of sink.

--- libs/nova/include/kmac/nova/version.h
+++ libs/nova/include/kmac/nova/version.h
@@ -2,11 +2,28 @@
 #ifndef KMAC_NOVA_VERSION_H
 #define KMAC_NOVA_VERSION_H
 
+#include <kmac/nova/platform/config.h>
+
 #include <cstddef>
 
-inline constexpr std::size_t NOVA_VERSION_MAJOR = 0;
-inline constexpr std::size_t NOVA_VERSION_MINOR = 1;
-inline constexpr std::size_t NOVA_VERSION_PATCH = 0;
-inline constexpr const char* NOVA_VERSION_STRING = "0.1.0";
+#define NOVA_VERSION_MAJOR_INT 0
+#define NOVA_VERSION_MINOR_INT 1
+#define NOVA_VERSION_PATCH_INT 0
+
+// NOLINTBEGIN(cppcoreguidelines-macro-usage)
+// two-level stringify forces macro expansion before stringification
+#define NOVA_VERSION_STRINGIFY( x ) #x
+#define NOVA_VERSION_TOSTRING( x ) NOVA_VERSION_STRINGIFY( x )
+
+#define NOVA_VERSION_STRING_LITERAL \
+	NOVA_VERSION_TOSTRING( NOVA_VERSION_MAJOR_INT ) "." \
+	NOVA_VERSION_TOSTRING( NOVA_VERSION_MINOR_INT ) "." \
+	NOVA_VERSION_TOSTRING( NOVA_VERSION_PATCH_INT )
+// NOLINTEND(cppcoreguidelines-macro-usage)
+
+NOVA_INLINE_VAR constexpr std::size_t NOVA_VERSION_MAJOR = NOVA_VERSION_MAJOR_INT;
+NOVA_INLINE_VAR constexpr std::size_t NOVA_VERSION_MINOR = NOVA_VERSION_MINOR_INT;
+NOVA_INLINE_VAR constexpr std::size_t NOVA_VERSION_PATCH = NOVA_VERSION_PATCH_INT;
+NOVA_INLINE_VAR constexpr const char* NOVA_VERSION_STRING = NOVA_VERSION_STRING_LITERAL;
 
 #endif // KMAC_NOVA_VERSION_H
